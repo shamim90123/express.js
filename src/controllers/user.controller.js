b80ac69c3c8237d9896078edb3d1d896964bde58
Md Shamim Reza <opt.shamim@gmail.com>
2025-07-04 22:41:49 +0600

    bulk users inert and list

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,41 @@
+const User = require('../models/user.model');
+const { faker } = require('@faker-js/faker');
+
+// Generate dummy users
+function generateUsers(count) {
+  const users = [];
+
+  for (let i = 0; i < count; i++) {
+    users.push({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      age: faker.number.int({ min: 18, max: 60 })
+    });
+  }
+
+  return users;
+}
+
+// Bulk insert controller
+exports.bulkInsertUsers = async (req, res) => {
+  try {
+    const users = generateUsers(50000);
+    await User.insertMany(users);
+    res.status(201).json({ message: '✅ 50,000 users inserted successfully!' });
+  } catch (error) {
+    console.error('❌ Insert failed:', error);
+    res.status(500).json({ error: 'Failed to insert users' });
+  }
+};
+
+
+// ✅ Get All Users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().limit(100); // limit to 100 users for now
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('❌ Failed to fetch users:', error);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user.controller');
 
-// GET /api/users/
+router.post('/bulk-insert', userController.bulkInsertUsers);
+
 router.get('/', (req, res) => {
   res.send('User route working!');
 });
+
+router.get('/user-list', userController.getAllUsers);
 
 module.exports = router;
