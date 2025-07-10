879a60e11fe7883b41b8845a2f296a77376dca65
Md Shamim Reza <opt.shamim@gmail.com>
2025-07-11 01:15:13 +0600

    update limit

--- src/config/rateLimit.js
+++ src/config/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1,
+  max: 100,
   message: 'Too many requests from this IP, please try again after 15 minutes',
   standardHeaders: true,
   legacyHeaders: false,
